2d94226a123f7a562e4214ac5e19bb281dd498b1
junpark-ai <sorijune@naver.com>
2024-10-29 17:13:15 +0900

    Adds "Isaac-Franka-Valve-Direct-v0" task

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/franka_cabinet/__init__.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/franka_cabinet/__init__.py
@@ -10,6 +10,7 @@ import gymnasium as gym
 
 from . import agents
 from .franka_cabinet_env import FrankaCabinetEnv, FrankaCabinetEnvCfg
+from .franka_valve_env import FrankaValveEnv, FrankaValveEnvCfg
 
 ##
 # Register Gym environments.
@@ -21,6 +22,18 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": FrankaCabinetEnvCfg,
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:FrankaCabinetPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="Isaac-Franka-Valve-Direct-v0",
+    entry_point="omni.isaac.lab_tasks.direct.franka_cabinet:FrankaValveEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": FrankaValveEnvCfg,
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:FrankaCabinetPPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
